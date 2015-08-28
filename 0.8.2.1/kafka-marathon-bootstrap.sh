ddd18bb07c324e147bbc3fc36c4030e02b8d0e62
Daniel Zukowski <daniel.zukowski@gmail.com>
2015-08-28 14:45:59 -0600

    wip

--- 0.8.2.1/kafka-marathon-bootstrap.sh
+++ 0.8.2.1/kafka-marathon-bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Get the last Host IP octet
-IFS=. read ip1 ip2 ip3 ip4 <<< "$HOST"
+IFS=. read ip1 ip2 ip3 ip4 <<< "$HOSTNAME"
 
 # Create unique broker.id as combination of last octet and the given Marathon Docker port
 broker_id="${ip4}${PORT0}"
 
 # Write custom-server.properties config file
 echo "broker.id=${broker_id}" > $KAFKA_HOME/config/custom-server.properties
-echo "host.name=${HOST}" >> $KAFKA_HOME/config/custom-server.properties
+echo "host.name=${HOSTNAME}" >> $KAFKA_HOME/config/custom-server.properties
 echo "port=${PORT0}" >> $KAFKA_HOME/config/custom-server.properties
 echo "log.dir=/var/log/kafka/kafka-${broker_id}" >> $KAFKA_HOME/config/custom-server.properties
 echo "zookeeper.connect=${KAFKA_ZOOKEEPER_CONNECT}" >> $KAFKA_HOME/config/custom-server.properties
